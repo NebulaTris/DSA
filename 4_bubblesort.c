12fc4cc315644ad5b453b4641e30679489fba629
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2022-12-26 17:48:01 +0530

    Create 4_bubblesort.c
    Write a program to implement bubble sort.

---
+++ 4_bubblesort.c
@@ -0,0 +1,34 @@
+//Aim:Write a program to implement bubble sort.
+//Code:
+
+#include <stdio.h>
+int main()
+{
+    int  n, j, i, swap;
+    printf("Enter number of elements:");
+    scanf("%d", &n);
+    int array[n];
+    printf("Enter %d integers:\n", n);
+    for (i= 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    for (i = 0 ; i < n - 1; i++)
+    {
+        for (j = 0 ; j < n - i- 1; j++)
+        { 
+            if (array[j] > array[j+1])
+            {
+                swap       = array[j];
+                array[j]   = array[j+1];
+                array[j+1] = swap;
+            }
+        }
+    }
+ 
+    printf("Sorted list in ascending order:\n");
+ 
+    for (i = 0; i < n; i++)
+        printf("%d\t", array[i]);
+    return 0;
+}
